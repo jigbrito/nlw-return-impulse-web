bcdbe2f8a7e541ac1a6cf28e40b7a21456037ef4
João Ignácio Nascimento de Brito <jaonascimentobrito@gmail.com>
2022-05-11 13:28:05 -0300

    first commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer'
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "311c23f77d4f0f",
+    pass: "838b7b0001ac6c"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'João Ignácio <jaonascimentobrito@gmail.com>',
+      subject,
+      html: body,
+    });
+  };
+}
